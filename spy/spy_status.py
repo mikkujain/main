46192bf0396f530481990314265e18c3ba81258f
Mikku jain <mikkujain22@gmail.com>
2018-04-07 10:05:34 +0530

    adding chats staus

--- spy/spy_status.py
+++ spy/spy_status.py
@@ -2,17 +2,23 @@ import sys
 import spy_friend
 from spy_details import Spy, chatMessage
 import csv
+import time
 
 STATUS_MESSAGE = []
+status_updated_msg=[]
+status_sent_time=time.ctime()
 def add_status(current_status_message):
     
     #print the current status message
     if current_status_message == None:
         print("You do not have any current status")
         print("You need to add a status")
+        #status_sent_time=
         message=input("Enter a status")
         if len(message)>0:
-            updated_status_message=message
+            status_updated_msg.append(message)
+            status_updated_msg.append(status_sent_time)
+            updated_status_message=str(status_updated_msg)
             STATUS_MESSAGE.append(updated_status_message)
     else:
         print("Your current status is " + current_status_message)
@@ -25,7 +31,9 @@ def add_status(current_status_message):
         new_status_message = input("Enter new status message: ")
         #Validate if the status message is not empty and append it to the list holding all the status messages
         if len(new_status_message) > 0:
-            updated_status_message = new_status_message
+            status_updated_msg.append(new_status_message)
+            status_updated_msg.append(status_sent_time)
+            updated_status_message = str(status_updated_msg)
             STATUS_MESSAGE.append(updated_status_message)
 
     elif update_choice.upper() == 'Y':
@@ -34,8 +42,13 @@ def add_status(current_status_message):
 
         message_selection = int(input("Choose from the older message"))
         if len(STATUS_MESSAGE) >= message_selection:
-            updated_status_message = STATUS_MESSAGE[message_selection-1]
-
+            read_status=[]
+            read_time=time.ctime()
+            read_status.append(STATUS_MESSAGE[message_selection-1])
+            read_status.append(read_time)
+            updated_status_message = str(read_status)
+            print("read staus time %s" %updated_status_message)
+            
     return updated_status_message
 
 def load_status():
